1036aa612a89cf1e57fe51d9f58cda2c71355e55
Eduardo <ideias.info@gmail.com>
2020-11-28 17:47:21 +0000

    New topics

--- replicator-server/src/main/java/br/com/replicatorserver/config/ReplicatorConfiguration.java
+++ replicator-server/src/main/java/br/com/replicatorserver/config/ReplicatorConfiguration.java
@@ -19,28 +19,46 @@ public class ReplicatorConfiguration {
     @Autowired
     private ElsConnector elsConnector;
 
-    @KafkaListener(topics = HUE_LIGHT_MSG, groupId = REPLICATOR_GROUP_ID)
+    @KafkaListener(topics = HUE_LIGHTS_MESSAGE, groupId = REPLICATOR_GROUP_ID)
     public void processHueLigths(@Payload final JsonNode msg){
-        logger.debug("replicating "+HUE_LIGHT_MSG+" msg");
-        elsConnector.addMsg(HUE_LIGHT_MSG, msg);
+        logger.debug("replicating "+HUE_LIGHTS_MESSAGE+" msg");
+        elsConnector.addMsg(HUE_LIGHTS_MESSAGE, msg);
     }
 
-    @KafkaListener(topics = SENSOR_STATE_CHANGE_MSG, groupId = REPLICATOR_GROUP_ID)
-    public void processSensorStateChange(@Payload final JsonNode msg){
-        logger.debug("replicating "+SENSOR_STATE_CHANGE_MSG+" msg");
-        elsConnector.addMsg(SENSOR_STATE_CHANGE_MSG, msg);
-    }
-
-    @KafkaListener(topics = LIGHT_STATE_CHANGE_MSG, groupId = REPLICATOR_GROUP_ID)
+    @KafkaListener(topics = LIGHT_STATE_CHANGE, groupId = REPLICATOR_GROUP_ID)
     public void processLightStateChange(@Payload final JsonNode msg){
-        logger.debug("replicating "+LIGHT_STATE_CHANGE_MSG+" msg");
-        elsConnector.addMsg(LIGHT_STATE_CHANGE_MSG, msg);
+        logger.debug("replicating "+LIGHT_STATE_CHANGE+" msg");
+        elsConnector.addMsg(LIGHT_STATE_CHANGE, msg);
     }
 
-    @KafkaListener(topics = TELEGRAM_MSG, groupId = REPLICATOR_GROUP_ID)
+    @KafkaListener(topics = PUSHOVER_MESSAGE, groupId = REPLICATOR_GROUP_ID)
+    public void processPushOverMessage(@Payload final JsonNode msg){
+        logger.debug("replicating "+PUSHOVER_MESSAGE+" msg");
+        elsConnector.addMsg(PUSHOVER_MESSAGE, msg);
+    }
+
+    @KafkaListener(topics = SENSOR_STATE_CHANGE, groupId = REPLICATOR_GROUP_ID)
+    public void processSensorStateChange(@Payload final JsonNode msg){
+        logger.debug("replicating "+SENSOR_STATE_CHANGE+" msg");
+        elsConnector.addMsg(SENSOR_STATE_CHANGE, msg);
+    }
+
+    @KafkaListener(topics = TELEGRAM_MESSAGE, groupId = REPLICATOR_GROUP_ID)
     public void processTelegramMessage(@Payload final JsonNode msg){
-        logger.debug("replicating "+TELEGRAM_MSG+" msg");
-        elsConnector.addMsg(TELEGRAM_MSG, msg);
+        logger.debug("replicating "+TELEGRAM_MESSAGE+" msg");
+        elsConnector.addMsg(TELEGRAM_MESSAGE, msg);
+    }
+
+    @KafkaListener(topics = TELEGRAM_MESSAGE_SENT, groupId = REPLICATOR_GROUP_ID)
+    public void processTelegramMessageSent(@Payload final JsonNode msg){
+        logger.debug("replicating "+TELEGRAM_MESSAGE_SENT+" msg");
+        elsConnector.addMsg(TELEGRAM_MESSAGE_SENT, msg);
+    }
+
+    @KafkaListener(topics = TEMPERATURE_STATUS, groupId = REPLICATOR_GROUP_ID)
+    public void processTemperatureStatus(@Payload final JsonNode msg){
+        logger.debug("replicating "+TEMPERATURE_STATUS+" msg");
+        elsConnector.addMsg(TEMPERATURE_STATUS, msg);
     }
 
 }
--- replicator-server/src/main/java/br/com/replicatorserver/utils/KafkaTopics.java
+++ replicator-server/src/main/java/br/com/replicatorserver/utils/KafkaTopics.java
@@ -2,10 +2,13 @@ package br.com.replicatorserver.utils;
 
 public class KafkaTopics {
 
-    public static final String HUE_LIGHT_MSG = "hue.lights.message";
-    public static final String SENSOR_STATE_CHANGE_MSG = "sensor.state.change";
-    public static final String LIGHT_STATE_CHANGE_MSG = "light.state.change";
-    public static final String TELEGRAM_MSG = "telegram.message";
+    public static final String HUE_LIGHTS_MESSAGE = "hue.lights.message";
+    public static final String LIGHT_STATE_CHANGE = "light.state.change";
+    public static final String PUSHOVER_MESSAGE = "pushover.message";
+    public static final String SENSOR_STATE_CHANGE = "sensor.state.change";
+    public static final String TELEGRAM_MESSAGE = "telegram.message";
+    public static final String TELEGRAM_MESSAGE_SENT = "telegram.message.sent";
+    public static final String TEMPERATURE_STATUS = "temperature.status";
 
     public static final String REPLICATOR_GROUP_ID = "replicator.group.id";
 }
